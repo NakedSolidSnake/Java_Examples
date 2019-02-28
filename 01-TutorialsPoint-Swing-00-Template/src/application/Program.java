333f2fcb44cddc5bf5ca0c011b36692fac47ef38
solidcris <cristianosstec@gmail.com>
2019-02-28 18:07:29 -0300

    template

---
+++ 01-TutorialsPoint-Swing-00-Template/src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import entities.SwingControl;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SwingControl control = new SwingControl();
+		control.showEventDemo();
+		
+	}
+
+}
