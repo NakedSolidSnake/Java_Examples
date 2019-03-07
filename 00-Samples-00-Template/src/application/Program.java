863d14d9d3a4802d487eff4267cbc33b4377efee
Cristiano <cristianosstec@gmail.com>
2019-03-07 19:32:14 -0300

    templates

---
+++ 00-Samples-00-Template/src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import javax.swing.JOptionPane;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String message = "Cristiano Silva de Souza";
+		String rep = message.replaceAll("a", "e");
+		JOptionPane.showMessageDialog(null, rep);
+
+	}
+
+}
