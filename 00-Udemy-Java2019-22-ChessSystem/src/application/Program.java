69e1fe116705db3c90240d8fefefdeb352a0b46d
NakedSolidSnake <cristianosstec@gmail.com>
2019-03-07 23:36:52 -0300

    Position Class add

--- 00-Udemy-Java2019-22-ChessSystem/src/application/Program.java
+++ 00-Udemy-Java2019-22-ChessSystem/src/application/Program.java
@@ -3,14 +3,19 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
+//		Locale.setDefault(Locale.US);
+//		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		sc.close();
 		
-		Scanner sc = new Scanner(System.in);
-		
-		sc.close();
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 
 }
