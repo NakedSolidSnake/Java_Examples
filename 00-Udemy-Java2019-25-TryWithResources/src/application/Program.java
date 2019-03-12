dbc7c269ebf1dfca168f966396f7228c170fc561
Cristiano <cristianosstec@gmail.com>
2019-03-12 20:14:00 -0300

    Try With Resources example

---
+++ 00-Udemy-Java2019-25-TryWithResources/src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String[] lines = new String[] {
+			"Free RTOS",
+			"Nuttx RTOS",
+			"Zephyr RTOS"
+		};
+		
+		String path = "/home/cssouza/out.txt";
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+			for(String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
