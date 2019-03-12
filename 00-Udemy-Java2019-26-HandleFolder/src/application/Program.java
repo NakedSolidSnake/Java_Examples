54f48827c77a38f8d4fa13c8504ca69668b57feb
Cristiano <cristianosstec@gmail.com>
2019-03-12 20:15:02 -0300

    Managing Folders example

---
+++ 00-Udemy-Java2019-26-HandleFolder/src/application/Program.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
+		File path = new File(strPath);
+		
+		//Listing folders
+		File[] folders = path.listFiles(File::isDirectory);
+		
+		System.out.println("FOLDERS:");
+		for(File folder : folders) {
+			System.out.println(folder);
+		}
+		
+		
+		//Listing files
+		File[] files = path.listFiles(File::isFile);
+		
+		System.out.println("FILES:");
+		for(File file : files) {
+			System.out.println(file);
+		}
+		
+		//Creating Dir
+		boolean success = new File(strPath + "/subdir").mkdir();
+		System.out.println(success);
+		
+		sc.close();
+		
+	}
+
+}
